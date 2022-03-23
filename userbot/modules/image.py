c1a404f0710c722ef2c0435b5159a183452fd75f
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2022-03-23 23:55:11 +0700

    Update image.py

--- userbot/modules/image.py
+++ userbot/modules/image.py
@@ -3,12 +3,12 @@
 # Ya gitu deh:')
 
 from shutil import rmtree
-from userbot.utils import kyy_cmd
+from userbot.utils import jim_cmd
 from userbot import CMD_HELP, CMD_HANDLER as cmd
 from userbot.utils import googleimagesdownload
 
 
-@kyy_cmd(pattern="img (.*)")
+@jim_cmd(pattern="img (.*)")
 async def goimg(event):
     query = event.pattern_match.group(1)
     if not query:
